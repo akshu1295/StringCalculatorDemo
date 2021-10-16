933a84cda6b6522151893aba11d153586f4b1f4f
sunny <shimpiakshay12@gmail.com>
2021-10-16 20:11:37 +0530

    test case for single number should return itself added

--- stringcalculator/src/main/java/com/tdd/stringcalculator/StringCalcTest.java
+++ stringcalculator/src/main/java/com/tdd/stringcalculator/StringCalcTest.java
@@ -4,11 +4,18 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+
 public class StringCalcTest {
 	@Test
     public void emptyStringShouldReturnZero() {
         StringCalcImpl sut = new StringCalcImpl();
         assertEquals(0, sut.calculate(""));
+    }
+	
+	@Test
+    public void oneNumberShouldReturnItself() {
+        StringCalcImpl sut = new StringCalcImpl();
+        assertEquals(3, sut.calculate("3"));
     }
 
 }
